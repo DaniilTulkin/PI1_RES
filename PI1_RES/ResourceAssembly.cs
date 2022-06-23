995fff3fcc9591d9558f7439581fb6cb3b452775
DaniilTulkin <tulkindaniilal@gmail.com>
2022-06-23 23:28:57 +0300

    Add project files.

---
+++ PI1_RES/ResourceAssembly.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace PI1_RES
+{
+    public static class ResourceAssembly
+    {
+        #region public methods
+
+        /// <summary>
+        /// Gets the current resource assembly.
+        /// </summary>
+        /// <returns></returns>
+        public static Assembly GetAssembly()
+        {
+            return Assembly.GetExecutingAssembly();
+        }
+
+        /// <summary>
+        /// Gets the namespace of the currently running resource assembly.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetNamespace()
+        {
+            return typeof(ResourceAssembly).Namespace + ".";
+        }
+
+        #endregion
+    }
+}
---
+++ PI1_RES/ResourceImage.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace PI1_RES
+{
+    /// <summary>
+    /// Gets the embedded resources image from the cbb.res assembly based on user provided file name with extension.
+    /// </summary>
+    public static class ResourceImage
+    {
+        #region public methods
+
+        public static BitmapImage GetIcon(string name)
+        {
+            // Create the resource reader stream.
+            var stream = ResourceAssembly.GetAssembly().GetManifestResourceStream(ResourceAssembly.GetNamespace() + "Images.Icons." + name);
+            var image = new BitmapImage();
+
+            // Contruct and return image
+            image.BeginInit();
+            image.StreamSource = stream;
+            image.EndInit();
+
+            // Return constructed BitmapImage
+            return image;
+        }
+
+        #endregion
+    }
+}
